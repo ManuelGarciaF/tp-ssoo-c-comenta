6e83826e863f4a658cb62ceeb240956088e26f19
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-05-03 18:25:09 -0300

    arreglar script

--- tmux_script.sh
+++ tmux_script.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Cerrar puertos
-sudo fuser -k 8002/tcp 8006/tcp 8007/tcp
+sudo fuser --kill 8002/tcp
+sudo fuser --kill 8006/tcp
+sudo fuser --kill 8007/tcp
 
 # Check if the session already exists
 if tmux has-session -t tp-ssoo 2>/dev/null; then
@@ -16,14 +18,14 @@ tmux rename-window "Memoria"
 tmux send-keys -t tp-ssoo:1 "cd memoria && make && make memcheck" C-m
 
 # Esperar que memoria inicie
-sleep 3
+sleep 1
 
 # Create the second window and run a command
 tmux new-window -t tp-ssoo:2 -n "CPU"
 tmux send-keys -t tp-ssoo:2 "cd cpu && make && make memcheck" C-m
 
 # Esperar que cpu inicie
-sleep 3
+sleep 2
 
 # Create the third window and run a command
 tmux new-window -t tp-ssoo:3 -n "Kernel"
