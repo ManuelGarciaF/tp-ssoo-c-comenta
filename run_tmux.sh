094ee41d3cb271bd06155c679ac90d3176ead3db
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-05-25 09:35:42 -0300

    sacar tests de la repo

--- run_tmux.sh
+++ run_tmux.sh
@@ -7,23 +7,34 @@ tmux new-session -d -s tp-ssoo
 tmux rename-window "Memoria"
 tmux send-keys -t tp-ssoo:1 "cd memoria && make && make memcheck" C-m
 
+
+tmux new-window -t tp-ssoo:2 -n "CPU"
+tmux send-keys -t tp-ssoo:2 "cd cpu && make" C-m
+
 # Esperar que memoria inicie
 sleep 1
 
-tmux new-window -t tp-ssoo:2 -n "CPU"
-tmux send-keys -t tp-ssoo:2 "cd cpu && make && make memcheck" C-m
+tmux send-keys -t tp-ssoo:2 "make memcheck" C-m
+
+
+tmux new-window -t tp-ssoo:3 -n "Kernel"
+tmux send-keys -t tp-ssoo:3 "cd kernel && make" C-m
 
 # Esperar que cpu inicie
 sleep 1
 
-tmux new-window -t tp-ssoo:3 -n "Kernel"
-# tmux send-keys -t tp-ssoo:3 "cd kernel && make && ./bin/kernel" C-m
-# tmux send-keys -t tp-ssoo:3 "cd kernel && make && make memcheck" C-m
-tmux send-keys -t tp-ssoo:3 "cd kernel && make && ../tests/finalizar_proceso_io.py | make memcheck" C-m
-sleep 1
+if [[ -z $1 ]]; then
+    tmux send-keys -t tp-ssoo:3 "make memcheck" C-m
+else
+    tmux send-keys -t tp-ssoo:3 "../$1 | make memcheck" C-m
+fi
 
 tmux new-window -t tp-ssoo:4 -n "IO"
 tmux send-keys -t tp-ssoo:4 "cd entradasalida && make" C-m
+
+# Esperar que kernel inicie
+sleep 1
+
 tmux send-keys -t tp-ssoo:4 "valgrind --leak-check=full ./bin/entradasalida Interfaz1 generica_ejemplo.config" C-m
 
 # Select the kernel window
